8ed9ca72269599058babd35e19a16947f13b200b
Oriol Closa <oriol.closa@est.fib.upc.edu>
2019-08-02 14:32:49 +0200

    Send HTML mails correctly

--- event/tasks.py
+++ event/tasks.py
@@ -2,14 +2,15 @@ from celery import shared_task
 from django.template.loader import render_to_string
 
 from app.enums import MailTag
-from app.utils import get_notification_template, send_email
+from app.utils import get_notification_template, send_email, get_substitutions_templates
 from app.variables import HACKATHON_DOMAIN, HACKATHON_NAME, HACKATHON_DESCRIPTION
 from event.models import Subscriber, Event
 
 
 @shared_task
 def send_subscriber_new(subscriber: Subscriber, event: Event = None):
-    context = dict(subscriber=subscriber, hk_domain=HACKATHON_DOMAIN, hk_name=HACKATHON_NAME, hk_description=HACKATHON_DESCRIPTION)
+    context = get_substitutions_templates()
+    context["subscriber"] = subscriber
     if event:
         context["event"] = event
     template = get_notification_template(method="email", type="subscribe", task="new", format="html")
@@ -26,7 +27,8 @@ def send_subscriber_new(subscriber: Subscriber, event: Event = None):
 
 @shared_task
 def send_subscriber_resubscribed(subscriber: Subscriber, event: Event = None):
-    context = dict(subscriber=subscriber, hk_domain=HACKATHON_DOMAIN, hk_name=HACKATHON_NAME, hk_description=HACKATHON_DESCRIPTION)
+    context = get_substitutions_templates()
+    context["subscriber"] = subscriber
     if event:
         context["event"] = event
     template = get_notification_template(method="email", type="subscribe", task="resubscribe", format="html")
