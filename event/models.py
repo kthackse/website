b77f8a5f626f88f4f0552e862c21c5ece90d0b74
Oriol Closa <oriol.closa@est.fib.upc.edu>
2019-08-11 18:27:38 +0200

    Add invoice send

--- event/models.py
+++ event/models.py
@@ -565,10 +565,10 @@ class Invoice(models.Model):
             messages["responsible_event"] = "An event responsible must be an organiser"
         if not self.responsible_company.is_sponsor:
             messages["responsible_company"] = "A company responsible must be a sponsor"
-        if self.responsible_event.company != self.responsible_company.company:
+        if self.responsible_event.company.id != self.responsible_company.company.id:
             messages[
                 "responsible_company"
-            ] = "The company responsible company must be the same"
+            ] = "The company responsible and company event must be the same"
         if messages:
             raise ValidationError(messages)
 
